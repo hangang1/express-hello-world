16a255cd72265d18f84a8c473357c133469404f2
im <ldh9904@gmail.com>
2025-02-03 19:24:46 +0900

    Feat: GET/login(Work in progress)

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 const express = require("express");
+const passport = require("passport");
 
 const index = require("./routes/index");
+const passportConfig = require("./passport");
+
+const authRouter = require("./rputes/auth");
 
 const app = express();
+passportConfig();
 
 app.use("/", index);
+
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+  }),
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use("/auth", authRouter);
 
 app.use(function (req, res, next) {
   const err = new Error("Not Found");
